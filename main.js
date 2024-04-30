ac6613c5574893036ce833e2dbb5144a8610980a
stfnrosa <rosaa.estefanii@gmail.com>
2024-04-30 16:44:53 -0300

    finalizando

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+    const elemento = document.querySelector(idElementoAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else{
+        console.log('Elemento não encontrado ou seletor inválido!')
+    }
+    
 }
 
 
-let contador = 0;
+for (let index = 0; index < listaDeTeclas.length; index++) {
+    const tecla = listaDeTeclas[index];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
 
-while (contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = tocaSom;
-    contador = contador + 1;
+    tecla.onkeydown = function  (evento){
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');            
+        }
+    }
+
+    tecla.onkeyup = function (evento){
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.remove('ativa');            
+        }
+    }
+    
 }
